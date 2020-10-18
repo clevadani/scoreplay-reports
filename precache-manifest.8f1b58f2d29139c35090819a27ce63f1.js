27a60dd3c09640f9b1b3eb164c0fe33813226523
Daniel Adetayo <daniel@flutterwavego.com>
2020-10-18 22:16:25 +0100

    Updates

---
+++ precache-manifest.8f1b58f2d29139c35090819a27ce63f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b555122aae43982fc6b4cd892ddc2e4a",
+    "url": "/scoreplay-reports/index.html"
+  },
+  {
+    "revision": "897a26301dc5f84caa49",
+    "url": "/scoreplay-reports/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "bfaa68df97b491e4cadb",
+    "url": "/scoreplay-reports/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "897a26301dc5f84caa49",
+    "url": "/scoreplay-reports/static/js/2.173804f6.chunk.js"
+  },
+  {
+    "revision": "28fb610804c07b2ae9198617d1a2284c",
+    "url": "/scoreplay-reports/static/js/2.173804f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfaa68df97b491e4cadb",
+    "url": "/scoreplay-reports/static/js/main.b08085c1.chunk.js"
+  },
+  {
+    "revision": "18e1bb3d1960ee347c81",
+    "url": "/scoreplay-reports/static/js/runtime-main.3cad38f5.js"
+  }
+]);
